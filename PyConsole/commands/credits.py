61c73f04b965c29c62db80ef1e6db6bd3432ca5c
KalicekGamer <99833699+KralicekGamerCz@users.noreply.github.com>
2023-07-20 22:20:25 +0200

    v2.0

---
+++ PyConsole/commands/credits.py
@@ -0,0 +1,6 @@
+from tkinter import messagebox
+
+
+def cmd():
+    messagebox.showinfo("Credits", "Děkuji za stáhnutí PyConsole. Toto je můj menší project. "
+                        "Koukni na můj web https://nejsem.online")
---
+++ PyConsole/commands/vymazat.py
@@ -0,0 +1,7 @@
+import os
+from commands.etc import system
+
+
+def cmd():
+    os.system('cls')
+    system.welcome()
