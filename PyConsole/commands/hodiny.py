cacb4d604df244b89574dccd4cc93f4fe4e6a4ba
KalicekGamer <99833699+KralicekGamerCz@users.noreply.github.com>
2023-09-04 10:13:46 +0200

    Delete PyConsole directory

--- PyConsole/commands/hodiny.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-
-
-def cmd():
-    webbrowser.open('https://nejsem.online/zive-hodiny.php')
--- PyConsole/commands/vymazat.py
+++
@@ -1,7 +0,0 @@
-import os
-from commands.etc import system
-
-
-def cmd():
-    os.system('cls')
-    system.welcome()
